bad88e8d0e40cbcddbce1c7cb4610b69aee79244
0JIEUN0 <jieun4436@naver.com>
2021-03-29 00:27:07 +0900

    Add Comment Component

--- week_02/src/App.js
+++ week_02/src/App.js
@@ -1,28 +1,80 @@
 import './App.css';
 import React, { Component } from 'react';
 
-function Post(props) {
+
+async function fetchEvent(url) {
+  const response = await fetch(url);
+  return await response.json();
+}
+
+class Post extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      id: this.props.PostInfo.id,
+      url: "https://jsonplaceholder.typicode.com/posts/"+ this.props.PostInfo.id +"/comments",
+      json: []
+    }
+  }
+  /*
+  const getComment = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1/comments");
+    setResult(response.json());
+  }
+  //fetchEvent("https://jsonplaceholder.typicode.com/posts/1/comments").map(re => console.log(re.email));
+  */
+
+  render() {
+    fetch(this.state.url)
+      .then(response => response.json())
+      .then(inputJson => this.setState(
+        {json: inputJson}
+      ));
+    return (
+      <div className="App-post">
+        <div className="Box">
+          <div className="Theme">User</div>
+          <div>{this.props.PostInfo.userId}</div>
+        </div>
+        <div className="Box">
+          <div className="Theme">Title</div>
+          <div>{this.props.PostInfo.title}</div>
+        </div>
+        <hr />
+        <div className="Box">
+          <div className="Theme">Body</div>
+          <div>{this.props.PostInfo.body}</div>
+        </div>
+        <hr />
+        <div>
+          {this.state.json.map(
+            comment => <Comment key={comment.id} CommentInfo={comment}></Comment>
+          )}
+        </div>
+      </div>
+    );  
+   }
+  /*
+  
+      {getComment().map(
+          comment => <Comment key={comment.id} CommentInfo={comment}></Comment>
+        )}
+        */
+}
+
+function Comment(props) {
   return (
-    <div className="App-post">
-      <div><strong>User</strong> {props.PostInfo.userId}</div>
-      <div><strong>Title</strong> {props.PostInfo.title}</div>
-      <div><strong>Body</strong> {props.PostInfo.body}</div>
+    <div className="Box">
+      <span className="Theme">ㄴ</span>
       <div>
+        <strong>{props.CommentInfo.name}</strong> {props.CommentInfo.email}<br />
+        {props.CommentInfo.body}
       </div>
     </div>
   );
 }
 
-function Comment(props) {
-  return (
-    <div>
-      <div><strong>{props.CommentInfo.name}</strong> {props.CommentInfo.email}</div>
-      <div>{props.CommentInfo.body}</div>
-    </div>
-  );
-}
-
-class App extends React.Component {
+class App extends Component {
   constructor(props){
     super(props);
     this.state = {
@@ -30,8 +82,6 @@ class App extends React.Component {
       json: []
     };  
   }
-
-
   render () {
     fetch(this.state.url)
       .then(response => response.json())
